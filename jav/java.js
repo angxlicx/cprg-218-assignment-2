9507fa7d16301ecefe8db68ca6cb2cbe8c4fa34b
angxlicx <kelly.hoang@edu.sait.ca>
2024-03-21 09:20:55 -0600

    hero container footer

---
+++ jav/java.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+window.onload = function() {
+    var currentYear = new Date().getFullYear();
+    var copyrightYearSpan = document.getElementById("copyrightYear");
+    copyrightYearSpan.textContent = currentYear;
+};
