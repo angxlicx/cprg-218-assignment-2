a68edee2e8c2f37da94a6fab8f752aeeb9363484
angxlicx <kelly.hoang@edu.sait.ca>
2024-03-21 20:44:40 -0600

    JS working

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,4 @@
+//accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,6 +13,8 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//footer copyright
 window.onload = function() {
     var currentYear = new Date().getFullYear();
     var copyrightYearSpan = document.getElementById("copyrightYear");
